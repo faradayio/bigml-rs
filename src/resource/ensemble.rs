533b1c2e08428645b75213f0f22d22a20843ef07
Derek Kastner <dkastner@gmail.com>
2017-08-10 13:55:42 -0500

    Add ensemble.importances

--- src/resource/ensemble.rs
+++ src/resource/ensemble.rs
@@ -1,8 +1,16 @@
 //! An ensemble of multiple predictive models.
 
+use std::collections::HashMap;
+
 use super::Resource;
 use super::id::*;
 use super::status::*;
+
+/// List of field codes mapped to input fields
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct EnsembleField {
+  pub name: String,
+}
 
 resource! {
     api_name "ensemble";
@@ -14,6 +22,14 @@ resource! {
     pub struct Ensemble {
         /// The current status of this ensemble.
         pub status: GenericStatus,
+
+        /// Maps BigML field codes to named input fields
+        /// This is just a single-entry dictionary of "fields" key mapping to
+        /// dictionary of fields
+        pub ensemble: HashMap<String, HashMap<String, EnsembleField>>,
+
+        /// Maps average importance per field (BigML field id => importance value).
+        pub importance: HashMap<String, f64>,
 
         // The dataset used to create this ensemble.
         //pub dataset: Id<Dataset>,
