689ad58339e420e94ac3297f1fec24cf52813b50
Eric Kidd <git@randomhacks.net>
2018-04-12 07:29:33 -0400

    Add some more fields to `dataset`

--- src/resource/dataset.rs
+++ src/resource/dataset.rs
@@ -1,8 +1,11 @@
 //! BigML dataset support.
 
+use std::collections::HashMap;
+
 use super::id::*;
 use super::status::*;
 use super::Resource;
+use super::source::Field;
 
 resource! {
     api_name "dataset";
@@ -14,5 +17,24 @@ resource! {
     pub struct Dataset {
         /// The current status of this execution.
         pub status: GenericStatus,
+
+        /// The number of columns in the dataset.
+        pub columns: usize,
+
+        /// Field IDs excluded when building this dataset.
+        pub excluded_fields: Vec<String>,
+
+        /// The number of fields of each type. This includes a few odd things
+        /// like "preferred", so we represent it as a string.
+        pub field_types: HashMap<String, u64>,
+
+        /// Metadata describing each field.
+        pub fields: HashMap<String, Field>,
+
+        /// Field IDs included when building this dataset.
+        pub input_fields: Vec<String>,
+
+        /// The number of rows in this dataset.
+        pub rows: usize,
     }
 }
